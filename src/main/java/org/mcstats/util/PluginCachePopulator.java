4fd36ef8e38d5129704b18274428ea1998b6e84c
Tyler Blair <hidendra@griefcraft.com>
2015-05-12 17:26:59 -0300

    Add util class to cache all plugins from db -> redis.

---
+++ src/main/java/org/mcstats/util/PluginCachePopulator.java
@@ -0,0 +1,33 @@
+package org.mcstats.util;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.mcstats.MCStats;
+import org.mcstats.model.Plugin;
+
+import java.util.List;
+
+/**
+ * Populates the cache with plugin data
+ */
+public class PluginCachePopulator {
+
+    private static final Logger logger = Logger.getLogger(PluginCachePopulator.class);
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        MCStats mcstats = MCStats.getInstance();
+        mcstats.init();
+
+        List<Plugin> plugins = mcstats.getDatabase().loadPlugins();
+
+        for (Plugin plugin : plugins) {
+            mcstats.getModelCache().cachePlugin(plugin);
+        }
+
+        logger.info("Cached " + plugins.size() + " plugins");
+
+        // TODO cache plugin graphs?
+    }
+
+}
